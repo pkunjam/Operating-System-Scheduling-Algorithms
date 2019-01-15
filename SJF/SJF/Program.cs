dd3242b59ff2f6536d2c17e48126b2649a0b1377
Punit Kunjam <punit17100@iiitnr.edu.in>
2019-01-16 01:15:59 +0530

    initial commit

---
+++ SJF/SJF/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SJF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, n;
+
+            Console.WriteLine("Enter the total no. of process :");
+            n = int.Parse(Console.ReadLine());
+
+            int[] wTime = new int[n];
+            int[] bTime = new int[n];
+            int[] TATime = new int[n];
+            int[] aTime = new int[n];
+
+            float avgWT = 0;
+            float avgTAT = 0;
+
+            Console.WriteLine("Enter the arrival time for corresponding processes :");
+            Console.WriteLine();
+
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("P" + (i + 1));
+                aTime[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Enter the burst time for corresponding processes :");
+
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("P" + (i + 1));
+                bTime[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+            }
+
+            Array.Sort(bTime);          // sorting burst time
+
+            wTime[0] = 0;    //waiting time for first process is 0
+
+            //calculating waiting time
+            for (i = 1; i < n; i++)
+            {
+                wTime[i] = 0;
+                for (j = 0; j < i; j++)
+                    wTime[i] += bTime[j];
+            }
+
+            //calculating turnaround time
+            for (i = 0; i < n; i++)
+            {
+                TATime[i] = bTime[i] + wTime[i];
+                avgWT += wTime[i];
+                avgTAT += TATime[i];
+            }
+
+            avgWT = avgWT / n;
+            avgTAT = avgTAT / n;
+            
+            Console.WriteLine("Process" + "  " + "Arrival time" + "  " + "Burst time" + "  " + "Turn around time" + "  " + "Waiting time");
+            Console.WriteLine();
+
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("P"+(i+1)+"\t     " + aTime[i] + "\t\t   " + bTime[i] + "\t\t" + TATime[i] + "\t\t" + wTime[i]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Average waiting time: " + avgWT);
+            Console.WriteLine("Average turn around time: " + avgTAT);
+        }
+    }
+}
